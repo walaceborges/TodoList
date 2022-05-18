790e62b30d02b021e9d19b4e28301c36ded8de85
Walace <walacewab@gmail.com>
2022-05-18 16:52:27 -0300

    feat: implementa as configuracoes inicias do projeto

---
+++ api/tests/getTask.test.js
@@ -0,0 +1,33 @@
+
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+import * as chaiHttp from 'chai-http';
+
+chai.use(chaiHttp);
+
+import { app } from '../app';
+import tasksMock from './mocks/tasks';
+
+const { expect } = chai;
+
+describe('GET /tasks', () => {
+  
+  it('Deve retornar todas as tasks', async () => {
+    const getAllStub = sinon.stub();
+    getAllStub.returns(tasksMock);
+
+    const controller = {
+      getAll: getAllStub
+    };
+
+    const server = app(controller);
+
+    const res = await chai.request(server)
+      .get('/tasks');
+
+    expect(res).to.have.status(200);
+    expect(res.body).to.be.an('array');
+    expect(res.body).to.have.length(3);
+  });
+
+});
---
+++ api/tests/mocks/tasks.js
@@ -0,0 +1,28 @@
+const tasksMock = [
+  {
+    id: 1,
+    title: 'Task 1',
+    description: 'Task 1 description',
+    status: 'pending',
+    createdAt: '2019-01-01T00:00:00.000Z',
+    updatedAt: '2019-01-01T00:00:00.000Z',
+  },
+  {
+    id: 2,
+    title: 'Task 2',
+    description: 'Task 2 description',
+    status: 'done',
+    createdAt: '2019-01-01T00:00:00.000Z',
+    updatedAt: '2019-01-01T00:00:00.000Z',
+  },
+  {
+    id: 3,
+    title: 'Task 3',
+    description: 'Task 3 description',
+    status: 'pending',
+    createdAt: '2019-01-01T00:00:00.000Z',
+    updatedAt: '2019-01-01T00:00:00.000Z',
+  },
+];
+
+export default tasksMock;
